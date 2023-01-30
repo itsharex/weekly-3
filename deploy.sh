2dc0f1fb6a87462e6987f41a3f4eed0dff635faf
howie6879 <howie6879@gmail.com>
2023-01-30 21:21:41 +0800

    Add mac-soft

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # echo "> Building HTML by mddocs..."
 # mkdocs build
-
+echo "> Update mac-soft"
+cd ./docs/soft/mac-soft && git pull
 echo "> Gen mkdocs config"
-pipenv run python main.py 
+cd ../../../
+pipenv run python main.py
 echo "> Push code to git"
 read -p "> Please input commit info: " commit_info
 git add *
